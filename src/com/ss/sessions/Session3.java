bb5cdf43370d9694154e5967a2f8a9c9cb9b733e
imsadanala <sadanalansp@gmail.com>
2021-09-05 17:49:20 +0530

    Selection Sort Example

--- src/com/ss/sessions/Session3.java
+++ src/com/ss/sessions/Session3.java
@@ -3,52 +3,31 @@ package com.ss.sessions;
 import java.util.Arrays;
 
 public class Session3 {
+
     public static void main(String[] args) {
-        Session3 test = new Session3();
-       // System.out.println(Arrays.toString(test.reverse(new int[]{6, 2, 7})));
-       // System.out.println(Arrays.toString(test.reverseWithTemp(new int[]{1, 2, 3})));
+        System.out.println(Arrays.toString(selectionSort(new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1})));
     }
 
     /**
-     * Outplace Algorithm
-     * TC: O(n)
-     * SC: O(1)
-     *
      * @param input
      * @return
      */
-    int[] reverse(int[] input) {
-        int[] inputRev = new int[input.length];
-        int endIndex = input.length - 1;
-        int startIndex = 0;
-        while (endIndex >= 0) {
-            inputRev[startIndex] = input[endIndex];
-            endIndex--;
-            startIndex++;
-        }
-        return inputRev;
-    }
-
-    /**
-     * Inplace Alogirthm
-     * TC: O(n)
-     * SC: O(1), no extra stackframe will be created
-     *
-     * @param input
-     * @return
-     */
-    int[] reverseWithTemp(int[] input) {
-        int temp;
-        int startIndex = 0;
-        int endIndex = input.length - 1;
-        while (startIndex < endIndex) {
-            temp = input[startIndex];
-            input[startIndex] = input[endIndex];
-            input[endIndex] = temp;
-            startIndex++;
-            endIndex--;
+    private static int[] selectionSort(int[] input) {
+        for (int i = 0; i < input.length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < input.length; j++) {
+                if (input[j] < input[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            if (i != minIndex) {
+                int temp = input[i];
+                input[i] = input[minIndex];
+                input[minIndex] = temp;
+            }
         }
         return input;
     }
+
 
 }
